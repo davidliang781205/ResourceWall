a6cca61f63c13ad80c5802953b52a0074ed471bb
davidliang781205 <davidliang781205@gmail.com>
2017-03-30 20:46:33 +0000

    First version of static HTML

--- public/scripts/post_toggle.js
+++ public/scripts/post_toggle.js
@@ -3,10 +3,10 @@ $(() => {
     var $title = $(this).find('.caption h3').text();
     var $description = $(this).find('.caption').children('.description').text();
     var $media = $(this).find('img').attr('src');
-    $('.modal-body').empty();
-    $('.modal-title').text($title);
-    $('.modal-body').append($('<img>').attr('src', $media));
-    $('.modal-body').append($description);
+    $('#myModal .modal-body').empty();
+    $('#myModal .modal-title').text($title);
+    $('#myModal .modal-body').append($('<img>').attr('src', $media));
+    $('#myModal .modal-body').append($description);
     $('#myModal').modal('toggle');
   });
 })
--- public/scripts/side_menu.js
+++ public/scripts/side_menu.js
@@ -1,4 +1,6 @@
-$("#menu-toggle").click(function(e) {
-  e.preventDefault();
-  $("#wrapper").toggleClass("toggled");
-});
+$(() => {
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+  });
+})
