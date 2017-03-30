e3cf884715cbac3ce4f31f1e05507f291d6f0387
davidliang781205 <davidliang781205@gmail.com>
2017-03-30 19:13:27 +0000

    Finished cards container

--- public/scripts/post_toggle.js
+++ public/scripts/post_toggle.js
@@ -1,6 +1,12 @@
-$(document).ready(() => {
-    $('.card').on('click', (e) => {
-        e.stopPropagation();
-        $('#myModal').modal('toggle');
-    })
+$(() => {
+  $('.card').click(function(e) {
+    var $title = $(this).find('.caption h3').text();
+    var $description = $(this).find('.caption').children('.description').text();
+    var $media = $(this).find('img').attr('src');
+    $('.modal-body').empty();
+    $('.modal-title').text($title);
+    $('.modal-body').append($('<img>').attr('src', $media));
+    $('.modal-body').append($description);
+    $('#myModal').modal('toggle');
+  });
 })
