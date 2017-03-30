55bc8b1b5bbfb5c225d942fda46e98225199868c
Georgi Nikolov <gnikolov@sfu.ca>
2017-03-30 22:21:30 +0000

    adding user_login server_side logic

--- routes/user_login.js
+++ routes/user_login.js
@@ -4,41 +4,28 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (knex) => {
-  // let user = request.cookies['userid'];
   let user = {
-    email: 'Bob@mar.ca',
-    password: "marBob"
+    email: req.body.email,
+    password: req.body.password
   }
-  // let verifiedUser = emailMatchesPassChec(user.email, user.password);
-  // if (verifiedUser){
-  //   req.session.user_id = user.id;
-  // }
   router.get("/", (req, res) => {
     knex
       .select("*")
       .from("users")
-      .where("users.email", "=", 'Bob@mar.ca')
-      // .andWhere("users.password", "=", emailMatchesPassCheck(user.email, user.password))
-      // .then((results) => {
-      //   res.json(results);
-      .then(comparePass)
-
-      // })
-    // });
+      .where("users.email", "=", req.body.email)
+      .then(result => {
+        comparePass(result);
+      })
   });
 
   return router;
 }
 function comparePass(obj){
-  console.log(obj, "user obj");
-}
-function emailMatchesPassCheck (email, password) {
- for (var key in users) {
-   if (users[key].email === email) {
-     if (bcrypt.compareSync(password, users[key].password)) {
-       return users[key];
-     }
-   }
- }
- return null;
+  return new Promise((resolve, reject) => {
+    if (bcrypt.compareSync(req.body.password, obj.password)) {
+      resolve(obj.id);
+    } else {
+      reject (obj);
+    }
+  });
 }
