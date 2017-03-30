3d308d87f1d9cc92778a6ef8974dd1377e583980
davidliang781205 <davidliang781205@gmail.com>
2017-03-30 23:06:46 +0000

    Finished Ajax login and register

--- public/scripts/side_menu.js
+++ public/scripts/side_menu.js
@@ -1,25 +1,40 @@
 $(() => {
-  $("#menu-toggle").on('click', (e) => {
+  $('#menu-toggle').on('click', (e) => {
     e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
+    $('#wrapper').toggleClass('toggled');
   });
 
-  $(".login-button").on('click', (e) => {
+  $('.login-button').on('click', (e) => {
+    var email = $('#login').find('.login-email').val();
+    var password = $('#login').find('.login-email').val();
     e.preventDefault();
     $.ajax({
       method: 'POST',
-      url: '/login',
-      data: textInput.serialize()
-    }).done(() => {});
+      url: '/api/user/login',
+      data: {
+        email: email,
+        password: password
+      }
+    }).done(() => {
+      window.location.reload();
+    })
   })
 
-  $(".register-button").on('click', (e) => {
+  $('.register-button').on('click', (e) => {
+    e.preventDefault();
+    var email = $('#register').find('.register-email').val();
+    var password = $('#register').find('.register-password').val();
     e.preventDefault();
     $.ajax({
       method: 'POST',
-      url: '/register',
-      data: textInput.serialize()
-    }).done(() => {});
+      url: '/api/user/register',
+      data: {
+        email: email,
+        password: password
+      }
+    }).done(() => {
+      window.location.reload();
+    });
   })
 
 })
