77b29225885d4789a902d2a0fd096bdee83da250
Georgi Nikolov <gnikolov@sfu.ca>
2017-03-30 23:11:12 +0000

    fixed login page and added the route with ajax

--- routes/user_login.js
+++ routes/user_login.js
@@ -2,26 +2,37 @@
 
 const express = require('express');
 const router  = express.Router();
+const bcrypt  = require('bcrypt');
+
 
 module.exports = (knex) => {
   router.get("/", (req, res) => {
+    const { email, password } = req.query;
+
     knex
       .select("*")
       .from("users")
-      .where("users.email", "=", req.body.email)
-      .then(result => {
-        comparePass(result);
-      })
+      .where("users.email", "=", email)
+      .then(comparePass(password))
+      .then(setSession)
+      .catch(err => console.err)
   });
 
   return router;
 }
-function comparePass(obj){
-  return new Promise((resolve, reject) => {
-    if (bcrypt.compareSync(req.body.password, obj.password)) {
-      resolve(obj.id);
-    } else {
-      reject (obj);
-    }
-  });
+function setSession(currentUser_id){
+  return req.session.id = currentUser_id;
+}
+function comparePass(password){
+  return function(users) {
+    console.log('my user password', users[0].password);
+    return new Promise((resolve, reject) => {
+      const isValidPassword = bcrypt.compareSync(password, users[0].password);
+      // if (isValidPassword) {
+        resolve(true);
+      // } else {
+      //   reject (false);
+      // }
+    });
+  }
 }
