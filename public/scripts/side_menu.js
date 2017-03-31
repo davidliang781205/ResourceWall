ccfab334704fb02df1d92d9102bfa8dca4b36fc3
davidliang781205 <davidliang781205@gmail.com>
2017-03-31 23:21:47 +0000

    Created a Create Post Form

--- public/scripts/side_menu.js
+++ public/scripts/side_menu.js
@@ -4,16 +4,4 @@ $(() => {
     $('#wrapper').toggleClass('toggled');
   });
 
-  // TODO: I think we're replacing this with a regular form submission?
-  // $('.login-button').on('click', (e) => {
-  //   var data = $('#login').serialize();
-  //   e.preventDefault();
-  //   $.ajax({
-  //     method: 'POST',
-  //     url: '/api/users/login',
-  //     data: data
-  //   }).done(() => {
-  //     window.location.reload();
-  //   })
-  // })
 })
