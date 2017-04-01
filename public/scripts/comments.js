8268eeac2424efdcdb6ecd245752e4c8a98dd224
davidliang781205 <davidliang781205@gmail.com>
2017-04-01 21:24:21 +0000

    Code cleaning

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -4,37 +4,16 @@ $(() => {
     event.preventDefault();
     let comment = $('#cardModal textarea').val();
     let urlid = $('#cardModal .post-meta').data('id');
-    console.log(comment);
-    console.log('metadata ', urlid);
 
     $.ajax({
-        method: 'post',
-        url: '/comments',
-        data: {
-          comment: comment,
-          urlid: urlid
-        }
-      }).done(function() {
-        console.log('hi');
-      })
-      //   loadTweets();
-      //   $('.all-tweets').removeClass('err');
-      //   $('section.new-tweet textarea').focus().val('');
-      // }).fail(function (err) {
-      //   $('.all-tweets').addClass('err');
-      // });
+      method: 'post',
+      url: '/comments',
+      data: {
+        comment: comment,
+        urlid: urlid
+      }
+    }).done(function() {
+      console.log('hi');
+    })
   });
-  // $('#cardModal .fa-thumbs-up').on('click', function (event) {
-  //   // let liked = false;
-  //   let count = 1;
-  //   $.ajax({
-  //     method: 'post',
-  //     url: '/likes',
-  //     data: count
-  //   }).done(function () {
-  //     // liked = true;
-  //   });
-  // });
-
-
 });
