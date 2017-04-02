99034a07e1eb25532620859867bcad27365a54aa
nikolovGeorgi <gnikolov@sfu.ca>
2017-04-02 21:02:08 +0000

    inserting rates in db tables

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -18,4 +18,139 @@ $(() => {
 
     })
   });
+
+  $('.card').click(function(e){
+    let rate = 0;
+    let urlid = $('#cardModal .post-meta').data('id');
+    $('.star-1').click(function(e){
+      let $this = $(this);
+      if($this.hasClass('highlight')) {
+        $this.removeClass('highlight');
+        // $.ajax({
+        //   method: 'post',
+        //   url: '/updateRates',
+        //   data:{
+        //     urlid:urlid
+        //   }
+        // })
+      } else {
+        $this.addClass('highlight');
+        rate = 5;
+        $.ajax({
+          method: 'post',
+          url: '/rates',
+          data: {
+            rate: rate,
+            urlid: urlid
+          }
+        }).done(function (){
+          console.log(('star5 clicked'));
+        })
+      }
+    })
+    $('.star-2').click(function(e){
+      let $this = $(this);
+      if($this.hasClass('highlight')) {
+        $this.removeClass('highlight');
+        // $.ajax({
+        //   method: 'post',
+        //   url: '/updateRates',
+        //   data:{
+        //     urlid:urlid
+        //   }
+        // })
+      } else {
+        $this.addClass('highlight');
+        rate = 4;
+        $.ajax({
+          method: 'post',
+          url: '/rates',
+          data: {
+            rate: rate,
+            urlid: urlid
+          }
+        }).done(function (){
+          console.log(('star4 clicked'));
+        })
+      }
+    })
+    $('.star-3').click(function(e){
+      let $this = $(this);
+      if($this.hasClass('highlight')) {
+        $this.removeClass('highlight');
+        // $.ajax({
+        //   method: 'post',
+        //   url: '/updateRates',
+        //   data:{
+        //     urlid:urlid
+        //   }
+        // })
+      } else {
+        $this.addClass('highlight');
+        rate = 3;
+        $.ajax({
+          method: 'post',
+          url: '/rates',
+          data: {
+            rate: rate,
+            urlid: urlid
+          }
+        }).done(function (){
+          console.log(('star3 clicked'));
+        })
+      }
+    })
+    $('.star-4').click(function(e){
+      let $this = $(this);
+      if($this.hasClass('highlight')) {
+        $this.removeClass('highlight');
+        // $.ajax({
+        //   method: 'post',
+        //   url: '/updateRates',
+        //   data:{
+        //     urlid:urlid
+        //   }
+        // })
+      } else {
+        $this.addClass('highlight');
+        rate = 2;
+        $.ajax({
+          method: 'post',
+          url: '/rates',
+          data: {
+            rate: rate,
+            urlid: urlid
+          }
+        }).done(function (){
+          console.log(('star2 clicked'));
+        })
+      }
+    })
+    $('.star-5').click(function(e){
+      let $this = $(this);
+      if($this.hasClass('highlight')) {
+        $this.removeClass('highlight');
+        // $.ajax({
+        //   method: 'post',
+        //   url: '/updateRates',
+        //   data:{
+        //     urlid:urlid
+        //   }
+        // })
+      } else {
+        $this.addClass('highlight');
+        rate = 1;
+        $.ajax({
+          method: 'post',
+          url: '/rates',
+          data: {
+            rate: rate,
+            urlid: urlid
+          }
+        }).done(function (){
+          console.log(('star1 clicked'));
+        })
+      }
+    })
+  });
 });
