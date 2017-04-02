1b586607f8321144022e6207716b937d88b84ba8
Kristin <kvcmorin@gmail.com>
2017-04-02 00:27:52 +0000

    still on search bar

--- routes/search_bar.js
+++ routes/search_bar.js
@@ -5,30 +5,38 @@ const router = express.Router();
 
 module.exports = (knex) => {
 
-  console.log("this is right inside search bar exports funct");
-  function searchWild(input, callback) {
-    return knex.select("*")
-      .from("urls")
-      .where({title: input})
-      .then((rows) => {
-        if (rows.length) {
-          return Promise.resolve(input);
-        } else {
-          return Promise.resolve();
-        }
-      })
+
+  function searchBar(input, callback) {
+    console.log("this is right inside search bar exports funct, input is: ", input);
+    return knex
+            .select()
+            .from("urls")
+            .where({ title: input})
+            .then((rows) => {
+              if (rows.length) {
+                return Promise.resolve(input);
+              } else {
+                return Promise.resolve();
+              }
+            })
   }
 
   router.post("/", (req, res) => {
 
     console.log("this is the req.body: ", req.body);
-     searchWild(req.body.searchInput, (err, input) => {
+     searchBar(req.body.searchInput, (err, input) => {
+      console.log("inside searchBar function");
       if (err) {
         console.error("ERROR:", err);
         return res.status(400).end();
       } else {
-        console.log("OK, search result is:", input);
-        res.redirect("/");
+        console.log("OK, no errors and function went through the search result is:", input);
+// this is where the results of the search results will go. Render into the index.ejs?
+//  ie) let ansFuncOfResults = function(req.body.searchInput)
+//  ie) templateVars = {searchResults: ansFuncOfResults}
+// use a for loop like what david did to display the urls on home in the index.ejs???
+        // res.render("index", templateVars);
+      res.redirect("/");
       }
     });
 
