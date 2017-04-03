6feb0483aebdb6455312ba371da09b95b91196a4
Kristin <kvcmorin@gmail.com>
2017-04-03 20:59:46 +0000

    comments didn't work but now they do

--- routes/home.js
+++ routes/home.js
@@ -3,46 +3,14 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = (query, knex) => {
-  let sessionEmail;
-  let sessionId;
-  let templateVars;
+module.exports = (query) => {
   router.get("/", (req, res) => {
+    let templateVars = {
+      user: req.session.user_id,
+      errors: req.flash("error"),
+      info: req.flash("info")
+    };
 
-// <<<<<<< HEAD
-//     console.log("welcome to get /userProfile knex query below");
-
-            //     // if (req.sessoin.user_id) {
-            //     //   let idNumb = Number(req.session.user_id);
-            //     //   knex("users")
-            //     //     .select("id", "email")
-            //     //     .where({id: idNumb})
-            //     //     .limit(1)
-            //     //     .then((rows) => {
-
-            //     //       sessionEmail = rows[0].email;
-            //     //       sessionId = rows[0].id;
-            //     //   });
-            //     // }
-
-      let templateVars = {
-        user: req.session.user_id,
-        errors: req.flash("error"),
-        info: req.flash("info"),
-        email: sessionEmail
-      };
-
-
-
-//       query.getLikesRates((e) => {
-//         templateVars.posts = e;
-//         query.getComments((e) => {
-//           templateVars.postComments = e;
-//           console.log("this is templateVars HOmepage: ", templateVars);
-//           res.render('index', templateVars);
-//           return;
-//         });
-// =======
     query.getLikesRates((e) => {
       templateVars.posts = e;
       query.getComments((e) => {
@@ -51,8 +19,8 @@ module.exports = (query, knex) => {
         console.log("this is templateVars HOmepage: ", templateVars);
         res.render('index', templateVars);
         return;
-        0c9f1d42b76d6db54cf39598cc0e761d5d783901
       });
+    });
   });
 
   return router;
