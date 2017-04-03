acb06bee6c96a0a3962e9a6c238249e9281a8a2b
nikolovGeorgi <gnikolov@sfu.ca>
2017-04-03 10:11:04 +0000

    fixing urls

--- db/seeds/comments_seed.js
+++ db/seeds/comments_seed.js
@@ -2,9 +2,11 @@ exports.seed = function(knex, Promise) {
   return knex('comments').del()
     .then(function () {
       return Promise.all([
-        knex('comments').insert({user_id: '2', url_id: '3', content: 'this is fnny'}),
-        knex('comments').insert({user_id: '1', url_id: '1', content: 'wtf'}),
-        knex('comments').insert({user_id: '2', url_id: '2', content: 'I dont even know why im here atm..'})
+        knex('comments').insert({user_id: '2', url_id: '3', content: 'this is fnny!'}),
+        knex('comments').insert({user_id: '1', url_id: '1', content: 'haha this is too funny'}),
+        knex('comments').insert({user_id: '2', url_id: '2', content: 'I dont even know why im here atm..'}),
+        knex('comments').insert({user_id: '2', url_id: '7', content: 'hahaha this is awesome'}),
+        knex('comments').insert({user_id: '3', url_id: '8', content: 'I wanna watch it!'})
       ]);
     });
 };
